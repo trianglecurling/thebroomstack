356a1ba66f89fe2a812be09cf4bf61c189c57977
Trevor Gau <trgau@microsoft.com>
2016-03-12 19:07:50 -0500

    Finish scraping program.

--- scraper/build/Release/schedule-scraper.js
+++ scraper/build/Release/schedule-scraper.js
@@ -1,4 +1,3 @@
-#!/bin/env node
 "use strict";
 var jsdom = require("jsdom");
 var request = require("request");
@@ -22,6 +21,10 @@ if (process.argv.indexOf("--fiddler") >= 0) {
 var site = "http://trianglecurling.com";
 var timeRE = /(\d\d?):(\d\d) ((?:P|A)M)/;
 var todaysGames = [];
+var skipRosters = false;
+if (process.argv.indexOf("--skip-rosters") >= 0) {
+    skipRosters = true;
+}
 console.log("Navigate to admin login screen...");
 request.get(site + "/administrator", {}, function (error, response, body) {
     console.log("Logging in...");
@@ -57,37 +60,200 @@ request.get(site + "/administrator", {}, function (error, response, body) {
                     jsdom.env(body, ["http://code.jquery.com/jquery.min.js"], function (errors, window) {
                         var $ = window.$;
                         var $table = $("table.table-striped");
-                        var $rows = $table.find("tbody tr");
-                        $rows.each(function (index, row) {
-                            var $row = $(row);
-                            var $timeCell = $row.find("td").eq(0);
-                            var timeColSpan = $timeCell.attr("colspan");
-                            if (timeColSpan && timeColSpan > 1) {
-                                return true;
-                            }
-                            var time = $timeCell.text();
-                            var _a = timeRE.exec(time), fullTime = _a[0], hour = _a[1], minute = _a[2], meridian = _a[3];
-                            var curDate = new Date();
-                            var dateTime = new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getDate(), parseInt(hour, 10) % 12 + (meridian === "PM" ? 12 : 0), parseInt(minute, 10));
-                            var sheet = $row.find("td").eq(1).text().trim();
-                            if (["A", "B", "C", "D"].indexOf(sheet) === -1) {
-                                return true;
-                            }
-                            var teams = $row.find("td").eq(2).text().trim();
-                            var _b = teams.split("vs").map(function (t) { return t.trim(); }), team1 = _b[0], team2 = _b[1];
-                            todaysGames.push({
-                                time: dateTime,
-                                sheet: sheet,
-                                team1: team1,
-                                team2: team2
+                        if ($table.length === 0) {
+                            console.log("There are no ice bookings in the schedule for today.");
+                        }
+                        else {
+                            var $rows = $table.find("tbody tr");
+                            $rows.each(function (index, row) {
+                                var $row = $(row);
+                                var $timeCell = $row.find("td").eq(0);
+                                var timeColSpan = $timeCell.attr("colspan");
+                                if (timeColSpan && timeColSpan > 1) {
+                                    return true;
+                                }
+                                var time = $timeCell.text();
+                                var _a = timeRE.exec(time), fullTime = _a[0], hour = _a[1], minute = _a[2], meridian = _a[3];
+                                var curDate = new Date();
+                                var dateTime = new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getDate(), parseInt(hour, 10) % 12 + (meridian === "PM" ? 12 : 0), parseInt(minute, 10));
+                                var sheet = $row.find("td").eq(1).text().trim();
+                                if (["A", "B", "C", "D"].indexOf(sheet) === -1) {
+                                    return true;
+                                }
+                                var teams = $row.find("td").eq(2).text().trim();
+                                var _b = teams.split("vs").map(function (t) { return t.trim(); }), team1 = _b[0], team2 = _b[1];
+                                todaysGames.push({
+                                    time: dateTime,
+                                    sheet: sheet,
+                                    team1: team1,
+                                    team2: team2
+                                });
                             });
-                        });
-                        console.log("Today's games: ");
-                        console.log(JSON.stringify(todaysGames, null, 4));
+                            if (!skipRosters) {
+                                var teamsToFetch = _.uniq(todaysGames.map(function (g) { return g.team1; }).concat(todaysGames.map(function (g) { return g.team2; })));
+                                console.log("Finished games. Now fetching teams rosters for " + teamsToFetch.join("; ") + "...");
+                                var teamScraper = new TeamScraper(stdHeaders);
+                                teamScraper.getTeams(getNextLeague().name, teamsToFetch).then(function (teams) {
+                                    for (var _i = 0, todaysGames_1 = todaysGames; _i < todaysGames_1.length; _i++) {
+                                        var game = todaysGames_1[_i];
+                                        game.team1 = teams[game.team1];
+                                        game.team2 = teams[game.team2];
+                                    }
+                                    console.log("Today's games: ");
+                                    console.log(JSON.stringify(todaysGames, null, 4));
+                                });
+                            }
+                            else {
+                                console.log("Today's games: ");
+                                console.log(JSON.stringify(todaysGames, null, 4));
+                            }
+                        }
                     });
                 });
             });
         });
     });
 });
+var leagues = [
+    {
+        name: "Sunday Morning Open - Winter 2016",
+        day: 0,
+        hour: 11,
+        minute: 0
+    },
+    {
+        name: "Sunday Doubles - Winter 2016",
+        day: 0,
+        hour: 18,
+        minute: 0
+    },
+    {
+        name: "Monday Spinner - Winter 2016",
+        day: 1,
+        hour: 19,
+        minute: 0
+    },
+    {
+        name: "Tuesday Competitive - Winter 2016",
+        day: 2,
+        hour: 19,
+        minute: 0
+    },
+    {
+        name: "Wednesday Open - Winter 2016",
+        day: 3,
+        hour: 18,
+        minute: 30
+    },
+    {
+        name: "Thursday Open - Winter 2016",
+        day: 4,
+        hour: 19,
+        minute: 0
+    }
+];
+function getNextLeague(from) {
+    if (from === void 0) { from = null; }
+    if (!from) {
+        var now = new Date(new Date().getTime() + 180 * 60 * 1000);
+        return getNextLeague(now);
+    }
+    var current = from;
+    for (var i = 0; i < 525600; ++i) {
+        var minute = current.getMinutes();
+        var hour = current.getHours();
+        var day = current.getDay();
+        for (var _i = 0, leagues_1 = leagues; _i < leagues_1.length; _i++) {
+            var league = leagues_1[_i];
+            if (league.day === day && league.hour === hour && league.minute === minute) {
+                return league;
+            }
+        }
+        current = new Date(current.getTime() + 60000);
+    }
+}
+var TeamScraper = (function () {
+    function TeamScraper(headers) {
+        this.headers = headers;
+        this.leagueIds = {};
+    }
+    TeamScraper.prototype.getTeams = function (leagueName, skips) {
+        var _this = this;
+        return this.ensureTeamIds().then(function () {
+            return Q.Promise(function (resolve, reject) {
+                console.log("Navigating to teams page for league " + leagueName + "...");
+                request.get(site + "/administrator/index.php?task=teams&option=com_curling&league_id=" + _this.leagueIds[leagueName], { headers: _this.headers }, function (error, response, body) {
+                    jsdom.env(body, ["http://code.jquery.com/jquery.min.js"], function (errors, window) {
+                        var $ = window.$;
+                        var $rows = $("table.table-striped tbody tr");
+                        var teamPromises = [];
+                        var _loop_1 = function(skip) {
+                            $rows.each(function (index, row) {
+                                var $cells = $(row).find("td");
+                                var teamSkip = $cells.eq(3).text().trim();
+                                if (teamSkip === skip) {
+                                    var url = $cells.eq(3).find("a")[0].href;
+                                    var teamPromise = _this.getTeam(url);
+                                    teamPromises.push(teamPromise);
+                                }
+                            });
+                        };
+                        for (var _i = 0, skips_1 = skips; _i < skips_1.length; _i++) {
+                            var skip = skips_1[_i];
+                            _loop_1(skip);
+                        }
+                        return Q.all(teamPromises).then(function (teams) {
+                            console.log("All teams discovered");
+                            var teamsObj = {};
+                            for (var _i = 0, teams_1 = teams; _i < teams_1.length; _i++) {
+                                var team = teams_1[_i];
+                                teamsObj[team.skip] = team;
+                            }
+                            resolve(teamsObj);
+                        });
+                    });
+                });
+            });
+        });
+    };
+    TeamScraper.prototype.getTeam = function (url) {
+        var _this = this;
+        return Q.Promise(function (resolve, reject) {
+            console.log("Getting requested team...");
+            request.get(url, { headers: _this.headers }, function (error, response, body) {
+                jsdom.env(body, ["http://code.jquery.com/jquery.min.js"], function (errors, window) {
+                    var $ = window.$;
+                    var name = $("input[name=team_name]").val();
+                    var skip = $("option[value=" + $("select").eq(1).val() + "]").eq(0).text();
+                    var vice = $("option[value=" + $("select").eq(2).val() + "]").eq(0).text();
+                    var second = $("option[value=" + $("select").eq(3).val() + "]").eq(0).text();
+                    var lead = $("option[value=" + $("select").eq(4).val() + "]").eq(0).text();
+                    console.log("Found team " + name);
+                    resolve({ name: name, lead: lead, second: second, vice: vice, skip: skip });
+                });
+            });
+        });
+    };
+    TeamScraper.prototype.ensureTeamIds = function () {
+        var _this = this;
+        if (this.leagueIds) {
+            return Q.resolve(null);
+        }
+        console.log("Discovering internal team ids...");
+        return Q.Promise(function (resolve, reject) {
+            request.get(site + "/administrator/index.php?task=teams&option=com_curling", { headers: _this.headers }, function (error, response, body) {
+                jsdom.env(body, ["http://code.jquery.com/jquery.min.js"], function (errors, window) {
+                    var $ = window.$;
+                    var $options = $("select[name=league_id] option");
+                    $options.each(function (index, element) {
+                        _this.leagueIds[$(element).text()] = parseInt($(element).attr("value"), 10);
+                    });
+                    console.log("Team ids found.");
+                    resolve(null);
+                });
+            });
+        });
+    };
+    return TeamScraper;
+}());
 //# sourceMappingURL=schedule-scraper.js.map
--- scraper/src/schedule-scraper.ts
+++ scraper/src/schedule-scraper.ts
@@ -47,6 +47,11 @@ const site = "http://trianglecurling.com";
 const timeRE = /(\d\d?):(\d\d) ((?:P|A)M)/;
 const todaysGames: Game[] = [];
 
+let skipRosters = false;
+if (process.argv.indexOf("--skip-rosters") >= 0) {
+	skipRosters = true;
+}
+
 console.log("Navigate to admin login screen...");
 request.get(`${site}/administrator`, {}, (error: any, response: http.IncomingMessage, body: any) => {
 	console.log("Logging in...");
@@ -94,38 +99,202 @@ request.get(`${site}/administrator`, {}, (error: any, response: http.IncomingMes
 					jsdom.env(body, ["http://code.jquery.com/jquery.min.js"], (errors: Error[], window: any) => {
 						const $ = window.$;
 						const $table = $("table.table-striped");
-						const $rows = $table.find("tbody tr");
-						$rows.each((index: number, row: any) => {
-							const $row = $(row);
-							let $timeCell = $row.find("td").eq(0);
-							const timeColSpan = $timeCell.attr("colspan");
-							if (timeColSpan && timeColSpan > 1) {
-								return true; // continue;
-							}
+						if ($table.length === 0) {
+							console.log("There are no ice bookings in the schedule for today.");
+						} else {
+							const $rows = $table.find("tbody tr");
+							$rows.each((index: number, row: any) => {
+								const $row = $(row);
+								let $timeCell = $row.find("td").eq(0);
+								const timeColSpan = $timeCell.attr("colspan");
+								if (timeColSpan && timeColSpan > 1) {
+									return true; // continue;
+								}
 
-							const time = $timeCell.text();
-							const [fullTime, hour, minute, meridian] = timeRE.exec(time);
-							const curDate = new Date();
-							const dateTime = new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getDate(), parseInt(hour, 10) % 12 + (meridian === "PM" ? 12 : 0), parseInt(minute, 10));
-							const sheet: string = $row.find("td").eq(1).text().trim();
-							if (["A", "B", "C", "D"].indexOf(sheet) === -1) {
-								return true; // continue
-							}
-							const teams = $row.find("td").eq(2).text().trim();
-							const [team1, team2] = teams.split("vs").map((t: string) => t.trim());
-							todaysGames.push({
-								time: dateTime,
-								sheet: sheet,
-								team1: team1,
-								team2: team2
+								const time = $timeCell.text();
+								const [fullTime, hour, minute, meridian] = timeRE.exec(time);
+								const curDate = new Date();
+								const dateTime = new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getDate(), parseInt(hour, 10) % 12 + (meridian === "PM" ? 12 : 0), parseInt(minute, 10));
+								const sheet: string = $row.find("td").eq(1).text().trim();
+								if (["A", "B", "C", "D"].indexOf(sheet) === -1) {
+									return true; // continue
+								}
+								const teams = $row.find("td").eq(2).text().trim();
+								const [team1, team2] = teams.split("vs").map((t: string) => t.trim());
+								todaysGames.push({
+									time: dateTime,
+									sheet: sheet,
+									team1: team1,
+									team2: team2
+								});
 							});
-						});
-						console.log("Today's games: ");
-						console.log(JSON.stringify(todaysGames, null, 4));
-					});
 
+							if (!skipRosters) {
+								const teamsToFetch = _.uniq(todaysGames.map(g => g.team1 as string).concat(todaysGames.map(g => g.team2 as string)));
+								console.log(`Finished games. Now fetching teams rosters for ${teamsToFetch.join("; ")}...`);
+								const teamScraper = new TeamScraper(stdHeaders);
+								teamScraper.getTeams(getNextLeague().name, teamsToFetch).then((teams) => {
+									for (const game of todaysGames) {
+										game.team1 = teams[game.team1 as string];
+										game.team2 = teams[game.team2 as string];
+									}
+									console.log("Today's games: ");
+									console.log(JSON.stringify(todaysGames, null, 4));
+								});
+							} else {
+								console.log("Today's games: ");
+								console.log(JSON.stringify(todaysGames, null, 4));
+							}
+						}
+					});
 				});
 			});
 		});
 	});
 });
+
+interface League {
+	name: string;
+	day: number;
+	hour: number;
+	minute: number;
+}
+
+const leagues: League[] = [
+	{
+		name: "Sunday Morning Open - Winter 2016",
+		day: 0,
+		hour: 11,
+		minute: 0
+	},
+	{
+		name: "Sunday Doubles - Winter 2016",
+		day: 0,
+		hour: 18,
+		minute: 0
+	},
+	{
+		name: "Monday Spinner - Winter 2016",
+		day: 1,
+		hour: 19,
+		minute: 0
+	},
+	{
+		name: "Tuesday Competitive - Winter 2016",
+		day: 2,
+		hour: 19,
+		minute: 0
+	},
+	{
+		name: "Wednesday Open - Winter 2016",
+		day: 3,
+		hour: 18,
+		minute: 30
+	},
+	{
+		name: "Thursday Open - Winter 2016",
+		day: 4,
+		hour: 19,
+		minute: 0
+	}
+];
+
+function getNextLeague(from: Date = null): League {
+	if (!from) {
+		// 3 hours in the future.
+		const now = new Date(new Date().getTime() + 180 * 60 * 1000);
+		return getNextLeague(now);
+	}
+
+	// This is me being super lazy.
+	let current = from;
+	for (let i = 0; i < 525600 /*1 year*/; ++i) {
+		const minute = current.getMinutes();
+		const hour = current.getHours();
+		const day = current.getDay();
+		for (const league of leagues) {
+			if (league.day === day && league.hour === hour && league.minute === minute) {
+				return league;
+			}
+		}
+		current = new Date(current.getTime() + 60000);
+	}
+}
+
+class TeamScraper {
+	private leagueIds: {[league: string]: number} = {};
+
+	constructor(private headers: any) { }
+
+	public getTeams(leagueName: string, skips: string[]): Q.Promise<{[skip: string]: Team}> {
+		return this.ensureTeamIds().then(() => {
+			return Q.Promise<{[skip: string]: Team}>((resolve, reject) => {
+				console.log(`Navigating to teams page for league ${leagueName}...`);
+				request.get(`${site}/administrator/index.php?task=teams&option=com_curling&league_id=${this.leagueIds[leagueName]}`, {headers: this.headers}, (error: any, response: http.IncomingMessage, body: any) => {
+					jsdom.env(body, ["http://code.jquery.com/jquery.min.js"], (errors: Error[], window: any) => {
+						const $ = window.$;
+						const $rows = $("table.table-striped tbody tr");
+						const teamPromises: Q.Promise<Team>[] = [];
+						for (const skip of skips) {
+							$rows.each((index: number, row: Element) => {
+								const $cells = $(row).find("td");
+								const teamSkip = $cells.eq(3).text().trim();
+								if (teamSkip === skip) {
+									const url = $cells.eq(3).find("a")[0].href;
+									const teamPromise = this.getTeam(url);
+									teamPromises.push(teamPromise);
+								}
+							});
+						}
+						return Q.all(teamPromises).then((teams) => {
+							console.log("All teams discovered");
+							const teamsObj: {[skip: string]: Team} = {};
+							for (const team of teams) {
+								teamsObj[team.skip] = team;
+							}
+							resolve(teamsObj);
+						});
+					});
+				});
+			});
+		});
+	}
+
+	private getTeam(url: string): Q.Promise<Team> {
+		return Q.Promise<Team>((resolve, reject) => {
+			console.log("Getting requested team...");
+			request.get(url, {headers: this.headers}, (error: any, response: http.IncomingMessage, body: any) => {
+				jsdom.env(body, ["http://code.jquery.com/jquery.min.js"], (errors: Error[], window: any) => {
+					const $ = window.$;
+					const name = $("input[name=team_name]").val();
+					const skip = $(`option[value=${ $("select").eq(1).val() }]`).eq(0).text();
+					const vice = $(`option[value=${ $("select").eq(2).val() }]`).eq(0).text();
+					const second = $(`option[value=${ $("select").eq(3).val() }]`).eq(0).text();
+					const lead = $(`option[value=${ $("select").eq(4).val() }]`).eq(0).text();
+					console.log(`Found team ${name}`);
+					resolve({name: name, lead: lead, second: second, vice: vice, skip: skip});
+				});
+			});
+		});
+	}
+
+	private ensureTeamIds(): Q.Promise<void> {
+		if (this.leagueIds) {
+			return Q.resolve<void>(null);
+		}
+		console.log("Discovering internal team ids...");
+		return Q.Promise<void>((resolve, reject) => {
+			request.get(`${site}/administrator/index.php?task=teams&option=com_curling`, {headers: this.headers}, (error: any, response: http.IncomingMessage, body: any) => {
+				jsdom.env(body, ["http://code.jquery.com/jquery.min.js"], (errors: Error[], window: any) => {
+					const $ = window.$;
+					const $options = $("select[name=league_id] option");
+					$options.each((index: number, element: Element) => {
+						this.leagueIds[$(element).text()] = parseInt($(element).attr("value"), 10);
+					});
+					console.log("Team ids found.");
+					resolve(null);
+				});
+			});
+		});
+	}
+}
